28ecf84483fae1c078e0b3f36e8f4fcdfb3e8435
Hans Vanpée <hans@vanpee.be>
2018-09-25 08:48:09 +0200

    Add correction

---
+++ correction/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Unit;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $articles = Article::all();
+        if ($request->session()->exists('status')) {
+            $status = $request->session('status');
+        }
+
+        return view('articles_index', compact('articles', 'status'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::orderBy('name')->pluck('name', 'id')->all();
+        $units = Unit::orderBy('name')->pluck('name', 'id')->all();
+
+        return view('article_form', compact('categories', 'units'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $newArticle = new Article($request->all());
+            $newArticle->save();
+            $request->session()->flash('status', "Article {$newArticle->name} ajouté");
+        } catch (Exception $e) {
+            $request->session()->flash('status', "L'article {$newArticle->name} n'a pas pu être ajouté");
+        }
+        return redirect(route('articles.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        return view('articles_details', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return redirect(route('articles.index'));
+    }
+}
---
+++ correction/resources/views/articles_index.blade.php
@@ -0,0 +1,34 @@
+@extends ('layout')
+
+@section('subheader', 'Liste des articles')
+@section ('content')
+    @isset($status)
+    {{ dd($status) }}
+    <div class="alert">
+        <p>{{ $status }}</p>
+    </div>
+    @endisset
+    <table class="table">
+        <tr>
+            <th>Article</th>
+            <th>Cat&eacute;gorie</th>
+            <th>Unit&eacute;</th>
+            <th>Prix de vente</th>
+            <th>Actions</th>
+        </tr>
+        @foreach($articles as $article)
+            <tr>
+                <td><a href="{{ route('articles.show', $article->id) }}">{{ $article->name }}</a></td>
+                <td>{{ $article->category->name }}</td>
+                <td>{{ $article->unit->name }}</td>
+                <td class="text-right">{{ $article->sales_price }}</td>
+                <td><form action="{{ route('articles.destroy', $article->id) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button>Supprimer</button>
+                    </form></td>
+            </tr>
+        @endforeach
+    </table>
+    <a href="{{ route('articles.create') }}">Ajouter</a>
+@endsection
